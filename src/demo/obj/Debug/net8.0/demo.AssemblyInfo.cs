631eb33a6cbd9aa414a87ebf02c1a8b3dd597269
Minh Nguyen <minh091222002@gmail.com>
2025-11-18 10:08:38 +0700

    cha bik cai nay la gi

--- src/demo/obj/Debug/net8.0/demo.AssemblyInfo.cs
+++ src/demo/obj/Debug/net8.0/demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6f817bad0959a951e6c9665541c4acbc7370ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a518769bb14744c94dd6d3fa497e65d45f6685")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
